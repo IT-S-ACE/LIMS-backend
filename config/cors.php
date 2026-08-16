0b249d632e2fd08d0b2affedbfc0ef2de8c8a7ad
ACE <hanan.a.masoud2003@gmail.com>
2026-08-17 01:32:37 +0300

    Add Netlify app to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
         'trim',
         explode(',', env(
             'CORS_ALLOWED_ORIGINS',
-            'http://localhost:3000,http://localhost:5173,http://127.0.0.1:3000,http://127.0.0.1:5173'
+            'http://localhost:3000,http://localhost:5173,http://127.0.0.1:3000,http://127.0.0.1:5173,https://lims-f.netlify.app'
         ))
     ))),
 
